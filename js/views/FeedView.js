47a0cce116282896a6e1f0b72a32db6ce538145e
Daniela Araque <daniaraque05@gmail.com>
2021-08-11 00:31:36 -0400

    cambio ruta de imagen

--- js/views/FeedView.js
+++ js/views/FeedView.js
@@ -5,10 +5,10 @@ export default () => {
   const feedView = `
   <nav id="navbar-feed">
     <div id="side-nav" >
-        <a href="#/feed" id="home-btn" class="redirect"><img class="vector" src = "/css/img_app/vector_home.png"></img></a>
-        <a id="match-btn" class="macth-redirect"><img class="match-vector" src = "/css/img_app/relationship.png"></img></a>
-        <a id="search-btn" class="redirect"><img class="vector" src = "/css/img_app/vector_search.png"></img></a>
-        <a href="#/profile" id="profile-btn" class="redirect"> <img class="vector" src = "/css/img_app/vector_profile.png"></a>
+        <a href="#/feed" id="home-btn" class="redirect"><img class="vector" src = "./css/img_app/vector_home.png"></img></a>
+        <a id="match-btn" class="macth-redirect"><img class="match-vector" src = "./css/img_app/relationship.png"></img></a>
+        <a id="search-btn" class="redirect"><img class="vector" src = "./css/img_app/vector_search.png"></img></a>
+        <a href="#/profile" id="profile-btn" class="redirect"> <img class="vector" src = "./css/img_app/vector_profile.png"></a>
     </div>
   </nav>
   
@@ -21,8 +21,8 @@ export default () => {
       </h1>
     </div>
     <div id="side-nav">
-        <a href="#/profile"  class="editBtn"><img class="vector" src = "/css/img_app/vector_edit_pink.png"></img></a>
-        <a href="#/profile" id="profile-btn-white" class="redirect"> <img class="vector" src = "/css/img_app/vector_profile.png"></a>
+        <a href="#/profile"  class="editBtn"><img class="vector" src = "./css/img_app/vector_edit_pink.png"></img></a>
+        <a href="#/profile" id="profile-btn-white" class="redirect"> <img class="vector" src = "./css/img_app/vector_profile.png"></a>
     </div>
   </nav>
   
@@ -30,7 +30,7 @@ export default () => {
     <div>
       <div id="header-logo">
         <div id="chevron-logo">
-          <img class="chevron-img" src="/css/img_app/chevronLS.png"</img>
+          <img class="chevron-img" src="./css/img_app/chevronLS.png"</img>
         </div>
       
          <div id="logoLS">
@@ -45,23 +45,23 @@ export default () => {
     </div>
     <div id="filter-icons">
         <div id="search-help">
-            <a><img class="search-help" src = "/css/img_app/help.png""></a>
+            <a><img class="search-help" src = "./css/img_app/help.png""></a>
             <p>Ayuda técnica</p>
         </div>
         <div id="search-memes">
-            <a><img class="search-memes" src = "/css/img_app/memes.png""></a>
+            <a><img class="search-memes" src = "./css/img_app/memes.png""></a>
             <p>Publica y ríete</p>
         </div>
         <div id="search-ghpage">
-            <a><img class="search-ghpage" src = "/css/img_app/GHpage.png""></a>
+            <a><img class="search-ghpage" src = "./css/img_app/GHpage.png""></a>
             <p>Proyecto Github</p>
         </div>
         <div id="search-resources">
-            <a><img class="search-resources" src = "/css/img_app/resources.png""></a>
+            <a><img class="search-resources" src = "./css/img_app/resources.png""></a>
             <p>Información útil</p>
         </div>
         <div id="search-team">
-            <a><img class="search-team" src = "/css/img_app/team.png""></a>            
+            <a><img class="search-team" src = "./css/img_app/team.png""></a>            
             <p>Encuentra compañera</p>
         </div>  
     </div>
@@ -77,23 +77,23 @@ export default () => {
         </div>
         <div id="filter-icons">
             <div id="search-help">
-                <a><img class="search-help" src = "/css/img_app/help.png""></a>
+                <a><img class="search-help" src = "./css/img_app/help.png""></a>
                 <p class="title-icon">Ayuda técnica</p>
             </div>
             <div id="search-memes">
-                <a><img class="search-memes" src = "/css/img_app/memes.png""></a>
+                <a><img class="search-memes" src = "./css/img_app/memes.png""></a>
                 <p class="title-icon">Publica y ríete</p>
             </div>
             <div id="search-ghpage">
-                <a><img class="search-ghpage" src = "/css/img_app/GHpage.png""></a>
+                <a><img class="search-ghpage" src = "./css/img_app/GHpage.png""></a>
                 <p class="title-icon">Proyecto Github</p>
             </div>
             <div id="search-resources">
-                <a><img class="search-resources" src = "/css/img_app/resources.png""></a>
+                <a><img class="search-resources" src = "./css/img_app/resources.png""></a>
                 <p class="title-icon">Información útil</p>
             </div>
             <div id="search-team">
-                <a><img class="search-team" src = "/css/img_app/team.png""></a>            
+                <a><img class="search-team" src = "./css/img_app/team.png""></a>            
                 <p class="title-icon">Encuentra compañera</p>
             </div>  
         </div>
@@ -147,9 +147,9 @@ export default () => {
         </form>
         
         <div class="tool-bar">
-          <img id="text-icon" class="textIcon" src="/css/img_app/vector_text.png"></img>
-          <img id="link-icon" class="linkIcon"src="/css/img_app/vector_link.png"></img>
-          <img id="addImg-icon" class="imgIcon" src="/css/img_app/vector_image.png"></img>
+          <img id="text-icon" class="textIcon" src="./css/img_app/vector_text.png"></img>
+          <img id="link-icon" class="linkIcon"src="./css/img_app/vector_link.png"></img>
+          <img id="addImg-icon" class="imgIcon" src="./css/img_app/vector_image.png"></img>
         </div>  
       </div>
     </div>
@@ -249,7 +249,7 @@ export default () => {
         //         ${post.text}
         //       </p>  
         //       <div class="interaction-bar">
-        //         <a><img class="btn-edit" id="edit-post" src= "/css/img_app/edit.png" data-id="${post.id}"></img></a>
+        //         <a><img class="btn-edit" id="edit-post" src= "./css/img_app/edit.png" data-id="${post.id}"></img></a>
         //       </div>
         //     </div>
         //     `;
@@ -337,11 +337,11 @@ export default () => {
               </p>  
               <div class="interaction-bar">
                 <div>
-                  <img class="btn-like" id="btn-like" src="/css/img_app/vector_like.png" data-id="${post.id}"></img>
+                  <img class="btn-like" id="btn-like" src="./css/img_app/vector_like.png" data-id="${post.id}"></img>
                   <p class="number-likes" id="counter-likes" data-id="${post.id}">${post.like.length} </p>
                 </div>
-                <a><img class="btn-edit" id="edit-post" src= "/css/img_app/edit.png" data-id="${post.id}"></img></a>
-                <a><img class="btn-delete" src= "/css/img_app/trash.png"data-id="${post.id}"></img></a>
+                <a><img class="btn-edit" id="edit-post" src= "./css/img_app/edit.png" data-id="${post.id}"></img></a>
+                <a><img class="btn-delete" src= "./css/img_app/trash.png"data-id="${post.id}"></img></a>
               </div>
             </div>
             `;
